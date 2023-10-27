140f3bef9a7e008ab4a9b7895187e57703be13f1
Ben Lantaff <ben@benlantaff.com>
2023-10-27 01:58:33 -0400

    added navigation bar at the top of the window; exit button works. added test-map to drive around on; camera follows player.

--- src/js/MainScene.js
+++ src/js/MainScene.js
@@ -7,16 +7,33 @@ export default class MainScene extends Phaser.Scene {
 
   preload() {
     Player.preload(this);
+    this.load.image("tiles", "../src/assets/images/bolosprites64.png");
+    this.load.tilemapTiledJSON("map", "../src/assets/maps/test-map.json");
   }
 
   create() {
+    const map = this.make.tilemap({ key: "map" });
+    this.map = map;
+    const tileset = map.addTilesetImage("bolosprites64", "tiles", 64, 64, 0, 0);
+
+    const bottom = map.createLayer("Bottom", tileset, 0, 0);
+    const top = map.createLayer("Top", tileset, 0, 0);
+
+    top.setCollisionByProperty({ collides: true });
+    bottom.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(top);
+    this.matter.world.convertTilemapLayer(bottom);
+
     this.player = new Player({
       scene: this,
-      x: 200,
-      y: 200,
+      x: 300,
+      y: 300,
       texture: "tank",
       frame: "tank1",
-    }).setScale(0.5);
+    }).setScale(0.85);
+
+    this.cameras.main.setBounds(0, 0, "100%", "100%");
+    this.cameras.main.startFollow(this.player, true, 1, 1);
   }
 
   update() {
